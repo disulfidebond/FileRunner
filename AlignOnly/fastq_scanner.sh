fe467701b669ee0866a959a5a1eaa158eaf1657e
John Caskey <disulfidebond@gmail.com>
2022-01-10 12:59:18 -0600

    Update fastq_scanner.sh

--- AlignOnly/fastq_scanner.sh
+++ AlignOnly/fastq_scanner.sh
@@ -1,11 +1,17 @@
 # simple watcher script
 while true ; do
-  bash scanDir_fastq.automated.sh
-  if [ $? -eq 0 ] ; then
-    bash createAndRun_AP.sh
+  TSTRING=$(date +"%m%d_%H%M")
+  source scanDir_fastq.automated.sh
+  STARTVAR=$?
+  if [ $STARTVAR -eq 0 ] ; then
+    INITIAL_TSTRING=$(echo "$TSTRING")
+    echo "created runFile.data.csv at $TSTRING" > runLog.processOnly.${INITIAL_TSTRING}.log
+    bash createAndRun_AP.sh 'hg19.fa' 'runFile.data.csv'
     while true ; do
       echo 'waiting for analysis to complete'
       if [ -f processOnly_run_completed ] ; then
+        TSTRING=$(date +"%m%d_%H%M")
+        echo "completed run at $TSTRING" >> runLog.processOnly.${INITIAL_TSTRING}.log
         sleep 10
         rm processOnly_run_completed
         break
@@ -15,5 +21,5 @@ while true ; do
   else 
     echo 'no fastq files found'
   fi
-  sleep 30m
+  sleep 4h
 done
