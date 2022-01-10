a618226124b9b5ae64b3e9c80f557535dfc37d14
John Caskey <disulfidebond@gmail.com>
2022-01-10 13:01:14 -0600

    Update fastq_scanner.sh

--- Mutect2/fastq_scanner.sh
+++ Mutect2/fastq_scanner.sh
@@ -1,11 +1,17 @@
 # simple watcher script
 while true ; do
-  bash scanDir_fastq.automated.sh
-  if [ $? -eq 0 ] ; then
-    bash createAndRun_AP_Mu2TO.sh
+  TSTRING=$(date +"%m%d_%H%M")
+  source scanDir_fastq.automated.sh
+  STARTVAR=$?
+  if [ $STARTVAR -eq 0 ] ; then
+    INITIAL_TSTRING=$(echo "$TSTRING")
+    echo "created runFile.data.csv at $TSTRING" > runLog.mu2to.${INITIAL_TSTRING}.log
+    bash createAndRun_AP_Mu2TO.sh 'hg19.fa' 'runFile.data.csv'
     while true ; do
       echo 'waiting for analysis to complete'
       if [ -f mu2to_run_completed ] ; then
+        TSTRING=$(date +"%m%d_%H%M")
+        echo "completed run at $TSTRING" >> runLog.mu2to.${INITIAL_TSTRING}.log
         sleep 10
         rm mu2to_run_completed
         break
@@ -15,5 +21,5 @@ while true ; do
   else 
     echo 'no fastq files found'
   fi
-  sleep 30m
+  sleep 4h
 done
